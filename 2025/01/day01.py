88b698242f1e80b1c6ab08743bece7d9b4232136
Michael Brown <michlfbrown@gmail.com>
2025-12-03 13:07:39 -0800

    2025 day 1 part 2

--- 2025/01/day01.py
+++ 2025/01/day01.py
@@ -1,3 +1,5 @@
+import math
+
 START = 50
 DIAL_MIN = 0
 DIAL_MAX = 100
@@ -22,7 +24,34 @@ def part_one(rotations: list[str]):
     return password
 
 
+def part_two(rotations: list[str]):
+    password = 0
+    current = START
+
+    for rotation in rotations:
+        direction = rotation[0]
+        amount = int(rotation[1:])
+        full_rotations = math.floor(amount / 100)
+        remainder = amount - full_rotations * 100
+        password += full_rotations
+
+        if direction == "L":
+            new = (current - remainder) % 100
+            if new == 0:
+                password += 1
+            elif current != 0 and new > current:
+                password += 1
+        else:
+            new = (current + remainder) % 100
+            if new < current:
+                password += 1
+
+        current = new
+    return password
+
+
 with open("input.txt", "r") as f:
     lines = f.read().splitlines()
 
 print(part_one(lines))
+print(part_two(lines))
