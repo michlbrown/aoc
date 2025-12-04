70b586b3a40b222d04e29c1dbb4586493c5c41fc
Michael Brown <michlfbrown@gmail.com>
2025-12-04 09:25:43 -0800

    2025 day 2 part 2

--- 2025/02/day02.py
+++ 2025/02/day02.py
@@ -12,6 +12,29 @@ def part1(ranges: list[tuple[int, int]]):
     return sum(invalid_ids)
 
 
+def part2(ranges: list[tuple[int, int]]):
+    invalid_ids = []
+
+    for id_range in ranges:
+        for id in range(id_range[0], id_range[1] + 1):
+            id = str(id)
+
+            for seq_len in range(1, len(id) // 2 + 1):
+                # Check if ID can be equally split
+                if len(id) % seq_len:
+                    continue
+
+                # Split ID into slices of size seq_len
+                slices = [id[i : i + seq_len] for i in range(0, len(id), seq_len)]
+                # Check if all slices are equal (repeated)
+                invalid = len(set(slices)) == 1
+                if invalid:
+                    invalid_ids.append(int(id))
+                    break
+
+    return sum(invalid_ids)
+
+
 with open("input.txt") as f:
     contents = f.readline().rstrip().split(",")
 
@@ -21,3 +44,4 @@ for s in contents:
     ranges.append((int(start), int(stop)))
 
 print(part1(ranges))
+print(part2(ranges))
