ccfefa757616eec2319e825b104b95c7f8869f01
Michael Brown <michlfbrown@gmail.com>
2025-12-04 13:55:28 -0800

    2025 day 3 part 3

--- 2025/03/day03.py
+++ 2025/03/day03.py
@@ -10,7 +10,34 @@ def part1(banks: list[str]):
     return total
 
 
+def part2(banks: list[str]):
+    total = 0
+    for bank in banks:
+        bank = list(bank)
+        NUM_BATTERIES = 12
+
+        # Initially the last 12 digits
+        maximized = bank[-NUM_BATTERIES:]
+        remaining = bank[:-NUM_BATTERIES]
+
+        # work backwards through battery bank
+        for b in reversed(remaining):
+            # if adding b would increase value
+            if b >= maximized[0]:
+                # find inflection point
+                i = 0
+                while i + 1 < NUM_BATTERIES and maximized[i] >= maximized[i + 1]:
+                    i += 1
+
+                maximized.pop(i)
+                maximized.insert(0, b)
+
+        total += int("".join(maximized))
+    return total
+
+
 with open("input.txt") as f:
     contents = [line.rstrip() for line in f.readlines()]
 
 print(part1(contents))
+print(part2(contents))
