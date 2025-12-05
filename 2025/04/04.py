2a95c0ded8ad8be7027b9568f63abecd5412a6ee
Michael Brown <michlfbrown@gmail.com>
2025-12-05 10:30:49 -0800

    day 4 part 2

--- 2025/04/04.py
+++ 2025/04/04.py
@@ -21,8 +21,35 @@ def part1(grid: list[list[str]]):
     return accessible_rolls
 
 
+def part2(grid: list[list[str]]):
+    total = 0
+
+    while True:
+        num_accessible = 0
+        accessible: list[tuple[int, int]] = []
+
+        for i, row in enumerate(grid):
+            for j, value in enumerate(row):
+                if value == "@":
+                    if is_accessible(grid, i, j):
+                        num_accessible += 1
+                        accessible.append((i, j))
+
+        # remove accessible rolls
+        for row, col in accessible:
+            grid[row][col] = "."
+
+        if num_accessible < 1:
+            break
+        else:
+            total += num_accessible
+
+    return total
+
+
 with open("input.txt") as f:
     contents = [line.rstrip() for line in f.readlines()]
     contents = [list(row) for row in contents]
 
 print(part1(contents))
+print(part2(contents))
