addac6b7261fec7e70192381bc8d5b7c52d97e52
Michael Brown <michlfbrown@gmail.com>
2025-12-07 15:44:45 -0800

    07 part 2

--- 2025/07/07.py
+++ 2025/07/07.py
@@ -1,3 +1,15 @@
+def part2(contents: list[str]):
+    beams = list(map(lambda i: 1 if i == "S" else 0, contents[0]))
+    for row in contents[2:]:
+        for i, val in enumerate(row):
+            if val == "^" and beams[i]:
+                beams[i - 1] += beams[i]
+                beams[i + 1] += beams[i]
+                beams[i] = 0
+
+    print(sum(beams))
+
+
 def part1(contents: list[str]):
     splits = 0
     beams = list(map(lambda i: True if i == "S" else False, contents[0]))
@@ -17,3 +29,4 @@ with open("input.txt") as f:
     contents = [line.rstrip() for line in f]
 
 part1(contents)
+part2(contents)
