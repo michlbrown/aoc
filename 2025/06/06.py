5c6f6be488399412a0c32f5b04decc0f9976600e
Michael Brown <michlfbrown@gmail.com>
2025-12-07 13:07:27 -0800

    Update 06.py

--- 2025/06/06.py
+++ 2025/06/06.py
@@ -1,6 +1,32 @@
 import math
 
 
+def part2(contents: list[str]):
+    # separate operands from operators
+    operators = contents[-1].split()
+    cols = contents[:-1]
+    cols = list(zip(*cols))  # rotate to get a column at each index
+
+    total = 0
+    operands = []
+    problem_num = 0
+    for col in cols:
+        if set(col) in ({" "}, {"\n"}):
+            match operators[problem_num]:
+                case "+":
+                    total += sum(operands)
+                case "*":
+                    total += math.prod(operands)
+
+            problem_num += 1
+            operands = []
+        else:
+            print(col)
+            operands.append(int("".join(col)))
+
+    print(total)
+
+
 def part1(contents: list[str]):
     total = 0
     lines = [line.split() for line in contents]
@@ -23,3 +49,4 @@ with open("input.txt") as f:
     contents = [line for line in f]
 
 part1(contents)
+part2(contents)
